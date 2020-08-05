33f1709caae8c94e89f9ffcdda19cca97c5f1ea4
Donish <nurakh99@gmail.com>
2020-08-06 03:12:23 +0500

    Pagination

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -9,9 +9,9 @@ from flask_login import login_user, current_user, logout_user, login_required
 
 
 @app.route('/')
-@app.route('/app')
 def index():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 
@@ -139,3 +139,11 @@ def delete_post(post_id):
     db.session.commit()
     flash('Deleted', 'success')
     return redirect(url_for('index'))
+
+
+@app.route('/user/<string:username>')
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('user_post.html', posts=posts, user=user)
